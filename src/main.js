000f236a65a5360bf188a26fdaf3db4e993dfe69
patyfil <patyfil@gmail.com>
2022-10-19 14:30:23 -0300

    add modificações

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import "./css/index.css"
+import IMask from "imask"
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
@@ -16,11 +17,58 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
-setCardType("visa")
+// Para trocar a cor do cartão:
+// setCardType("visa")
 //para executar no DOM: COMENTAR A LINHA DE CIMA E DESCOMENTAR A DE BAIXO
 // globalThis.setCardType = setCardType
 // globalThis.setCardType("visa")
 // globalThis.setCardType("mastercard")
 // globalThis.setCardType("default")
 
+// security code
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
 
+// expiration date
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      // from: new Data().getFullYear,
+      from: String(new Date().getFullYear()).slice(2), // PARA PEGAR SÓ 2 DÍGITOS DO ANO
+      to: String(new Date().getFullYear() + 10).slice(2), // PARA LIMITAR A 10 ANOS
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+// card number
+const cardNumber = document.querySelector("#card-numer")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+}
---
+++ src/regrasCartao.js
@@ -0,0 +1,16 @@
+// VISA 
+// ^4\d{0,15}
+// inicia com 4 seguido de mais 15 dígitos
+4234234423432344
+
+// MASTER 
+// (^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}
+// inicia com 5, seguido de um dígito entre 1 a 5, seguido de mais 2 dígitos
+// OU
+// inicia com 22, seguido de um dígito entre 2 e 9, seguido de mais 1 dígito
+// OU
+// inicia com 2, seguido de um dígito entre 3 e 7, seguido de mais 2 dígitos
+// seguido de mais 12 dígitos
+5353535353535353
+2323232323232323
+2234344655455664
